aab35f64d29e2c60a841ec23fade01b5b557535e
katawutp <katawut.paramee@gmail.com.com>
2024-11-28 14:58:18 +0700

    thirdweb.ts file added for ThirdwebProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThirdwebProvider } from "thirdweb/react";
+import { ThirdwebProvider } from "@/app/thirdweb";
 
 const inter = Inter({ subsets: ["latin"] });
 
